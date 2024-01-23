b3b63f69a6c8aabaaf6c27177c066641241c0d20
SayidolimNUU <127468262+SayidolimNUU@users.noreply.github.com>
2024-01-23 21:58:37 +0500

    Update task_1.cpp

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -1,9 +1,34 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Sayidolimxon Usmanxonov
+ * Date: 23.01.2024
  */
 
-class Problem1 {
-public:
-};
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int arraySize, target, sum = 0, c = 0, k;
+    cout << "Enter array size:" << endl;
+    cin >> arraySize;
+    int array[arraySize];
+    cout << "Enter the array:" << endl;
+    for (int i = 0; i < arraySize; ++i) cin >> array[i];
+    cout << "Enter the target:" << endl;
+    cin >> target;
+    for (int i = 1; i < pow(2, arraySize); ++i) {
+        k = i;
+        for (int j = 0; j < arraySize; ++j) {
+            if (k % 2) sum += array[j];
+            else sum -= array[j];
+            k /= 2;
+        }
+        if (sum == target) {
+            ++c;
+        }
+        sum = 0;
+    }
+    cout << c << endl;
+    return 0;
+}
